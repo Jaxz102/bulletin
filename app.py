51bbf1ee285339c696813004e5f86b6165b307b3
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2021-05-01 16:51:48 -0400

    More Frontend Updates

--- app.py
+++ app.py
@@ -7,6 +7,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# LOGIN
+@app.route("/login") #HOME BUTTON PRESSED
+def loginFront():
+    return render_template("login.html")
+
+# ADMIN
+@app.route("/admin", methods=['POST', 'GET']) #HOME BUTTON PRESSED
+def admin():
+
+    if request.method == "POST":
+        if request.form["username"] == "admin" and request.form["password"] == "1234":
+            return render_template("admin.html")
+        else:
+            return render_template("login.html", login_status = "False")
+
+    return render_template("index.html", login_status = "True")
+
+# ADD EVENTS
+@app.route("/addevents") #HOME BUTTON PRESSED
+def add():
+    return render_template("add.html")
+
+
 #pip3 install virtualenv
 #virtualenv env
 #source env/bin/activate
